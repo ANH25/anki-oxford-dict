726fe38a61d37eb18de62f1fb46a02c7b92e34e2
ANH <abd.nh25@gmail.com>
2020-07-26 16:01:47 +0300

    add option to change shortcut & small fixes

--- __init__.py
+++ __init__.py
@@ -12,7 +12,17 @@ config = mw.addonManager.getConfig(__name__)
 addon_dir = os.path.dirname(__file__)
 odict = OxfordDict(config['app_id'], config['app_key'])
 
+def process_audio(editor, url, phonetic):
+    filename = editor._retrieveURL(url)
+    if not filename:
+        showWarning(f"Could not download {url}")
+        return ""
+    return f"[sound:{filename}] {phonetic}<br>"
+
 def populate_fields(editor):
+
+    if type(editor.currentField) != int:
+        return
 
     config = mw.addonManager.getConfig(__name__)
     fields = editor.note.values()
@@ -34,6 +44,10 @@ def populate_fields(editor):
             warning += f"{i} = {config[i]}\n"
         showWarning(warning)
         return
+    if def_field_i >= len(fields):
+        def_field_i = -1
+    if audio_field_i >= len(fields):
+        audio_field_i = -1
     if def_field_i < 0 and audio_field_i < 0:
         return
 
@@ -45,8 +59,8 @@ def populate_fields(editor):
         return
 
     added_audio = []
-    prons = []
     def_field = f"{fields[def_field_i]} "
+    audio_field = f"{fields[audio_field_i]} "
 
     for res in results:
         lex_entries = res['lexicalEntries']
@@ -56,8 +70,7 @@ def populate_fields(editor):
                 pronunciations = lex_ent.get('pronunciations', [])
                 for pr in pronunciations:
                     if 'audioFile' in pr.keys() and pr['audioFile'] not in added_audio:
-                        phonetic_spelling = pr.get('phoneticSpelling', '')
-                        prons.append([pr['audioFile'], phonetic_spelling])
+                        audio_field += process_audio(editor, pr['audioFile'], pr.get('phoneticSpelling', ''))
                         added_audio.append(pr['audioFile'])
                 else:
                     entries = lex_ent.get('entries', [])
@@ -65,8 +78,7 @@ def populate_fields(editor):
                         pronunciations = ent.get('pronunciations', [])
                         for pr in pronunciations:
                             if 'audioFile' in pr.keys() and pr['audioFile'] not in added_audio:
-                                phonetic_spelling = pr.get('phoneticSpelling', '')
-                                prons.append([pr['audioFile'], phonetic_spelling])
+                                audio_field += process_audio(editor, pr['audioFile'], pr.get('phoneticSpelling', ''))
                                 added_audio.append(pr['audioFile'])
 
             if def_field_i >= 0:
@@ -85,26 +97,21 @@ def populate_fields(editor):
 
     if def_field_i >= 0:
         editor.note.__setitem__(field_names[def_field_i], def_field)
-
     if audio_field_i >= 0:
-        audio_field = f"{fields[audio_field_i]} "
-        for pron in prons:
-            filename = editor._retrieveURL(pron[0])
-            if not filename:
-                showWarning(f"Could not download {pron[0]}")
-                continue
-            audio_field += f"[sound:{filename}] {pron[1]} <br>"
         editor.note.__setitem__(field_names[audio_field_i], audio_field)
 
     editor.loadNote()
     editor.checkValid()
 
 def add_button(buttons, editor):
+    shortcut = config['shortcut']
+    if not shortcut:
+        shortcut = "Ctrl+Shift+O"
     buttons.append(editor.addButton(
         os.path.join(addon_dir, 'icon.ico'),
         "oxford_dict",
         populate_fields,
-        tip = "Oxford Dict (Ctrl+Shift+O)",
-        keys = "Ctrl+Shift+O"))
+        tip = f"Oxford Dict ({shortcut})",
+        keys = shortcut))
 
 gui_hooks.editor_did_init_buttons.append(add_button)
--- config.md
+++ config.md
@@ -1,3 +1,4 @@
 * **lang**: Language. use `"en-gb"` for British English, `en-us` for American English.
 * **app_id**, **app_key**: you can get your own keys from <https://developer.oxforddictionaries.com>.
 * **def_field**, **audio_field**: indices of fields to put definitions and pronunciations into (use -1 to turn off).
+* **shortcut**: shortcut to activate the add-on.
